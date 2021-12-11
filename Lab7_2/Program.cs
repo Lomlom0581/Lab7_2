3e6ea988390ad423106f24472ae4ccc30ecda522
Антон Еремеев <kzanton_81@mail.ru>
2021-12-12 01:42:49 +0300

    Добавьте файлы проекта.

---
+++ Lab7_2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab7_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine(@"Создать метод для вычисления объема и площади поверхности куба по длине его ребра.");
+            Console.WriteLine("");
+            Console.Write("Введите ребро куба:");
+            double Side = double.Parse(Console.ReadLine());
+
+             
+            GetCubeVolumeAndSurfacArea(Side, out double SurfaceArea, out double Volume);
+
+            Console.WriteLine($"Объём куба:{Volume} , площадь поверхности: {SurfaceArea}");
+
+            Console.ReadKey();
+        }
+    }
+}
